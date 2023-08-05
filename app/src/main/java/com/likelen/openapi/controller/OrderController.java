6fe9459d6ab3e5bcaaed393ac28d2c33359307ca
lenkim <joenggyu0@gmail.com>
2023-08-05 14:58:09 +0900

    fix: confirm api working well

--- app/src/main/java/com/likelen/openapi/controller/OrderController.java
+++ app/src/main/java/com/likelen/openapi/controller/OrderController.java
@@ -3,10 +3,12 @@ package com.likelen.openapi.controller;
 import com.likelen.openapi_demo.apis.OrderApi;
 import com.likelen.openapi_demo.models.SubscriptionOrdersModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 class OrderController implements OrderApi {
 
     @Override
